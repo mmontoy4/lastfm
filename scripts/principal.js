dda7053962fad28913b31943806959a856f93a3a
mmontoy4 <mmontoya@almata.cat>
2021-05-17 19:19:58 +0200

    obro la nova branca develop ja que ja he aconseguit la connexio

---
+++ scripts/principal.js
@@ -0,0 +1,8 @@
+var myAPI_key="062e9a51720da9c3c4e2bce4aaf4e5ab";
+var myshared_secret="d28efddba401878b2c80f6f252f43a65";
+
+var url = window.location.href; // or window.location.href for current url
+var captured = /token=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
+var result = captured ? captured : 'myDefaultValue';
+
+console.log(captured);
