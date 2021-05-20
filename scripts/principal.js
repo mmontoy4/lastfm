7178c00e9c5a71f98e332b4c23870387c1c722fd
mmontoy4 <mmontoya@almata.cat>
2021-05-20 17:18:14 +0200

    fa la carrega de api sig i fa consultes en json

--- scripts/principal.js
+++ scripts/principal.js
@@ -4,7 +4,10 @@ var myshared_secret="d28efddba401878b2c80f6f252f43a65";
 var url = window.location.href; // or window.location.href for current url
 var captured = /token=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
 var result = captured ? captured : 'myDefaultValue';
-
+let nomArtista = null;
+let mbid = null;
+let nomAlbum = null;
+let mbidAlbum = null;
 //console.log(captured);
 
 //Calcular API SIG
@@ -46,7 +49,7 @@ function calculateApiSig(params) {
 
 //PETICIÓN
 
-function calculateApiSignatureStack() {
+$(document).ready(function(){
 
     // Set elsewhere but hacked into this example:
   //  var apisigGetSession;
@@ -86,8 +89,7 @@ function calculateApiSignatureStack() {
             console.log('Error - ' + errorMessage);
         }
     });
-}
-
+});
   function infoArtista(){
 
     var data = {
@@ -97,7 +99,7 @@ function calculateApiSignatureStack() {
         'autocorrect' : 1,
          'method': 'artist.getinfo'
     };
-
+    //console.log(tupu);
     //data["api_sig"] = calculateApiSig( data);
 
     data["format"] = "json";
@@ -116,12 +118,77 @@ function calculateApiSignatureStack() {
          //   data['method'] = res;
         //->     last_fm_data[method] = res;
            // var  myresposta = JSON.parse(res);
-            console.log("Resposta: Name " + res.artist.name);// Should return session key.
-            console.log("Resposta: Key " + res.artist.mbid);
+            //console.log("Resposta: Name " + res.artist.name);// Should return session key.
+            //console.log("Resposta: Key " + res.artist.mbid);
+            nomArtista = res.artist.name;
+            mbid = res.artist.mbid;
 
             //store session key for further authenticate operations...
             sessionStorage.setItem("mySessionUser", res.artist.name);
             sessionStorage.setItem("mySessionKey", res.artist.mbid);
+            $("#nomArtista").text("Nom: "+res.artist.name);
+            $("#mbidArtista").text("MBID: "+res.artist.mbid);
+        },
+        error : function(xhr, status, error){
+            var errorMessage = xhr.status + ': ' + xhr.statusText
+            console.log('Error - ' + errorMessage);
+        }
+    });
+
+  }
+
+  function millorsAlbumsArtista(){
+    var data = {
+        'token': Utf8.encode(captured),
+        'api_key': "062e9a51720da9c3c4e2bce4aaf4e5ab",
+        'artist' : "Doja Cat",
+        'autocorrect' : 1,
+         'method': 'artist.getTopAlbums'
+    };
+
+    //data["api_sig"] = calculateApiSig( data);
+
+    data["format"] = "json";
+
+    //console.log("DATa", data);
+    var last_url="http://ws.audioscrobbler.com/2.0/?";
+
+    $.ajax({
+        type: "GET",
+        url: last_url,
+        data:data,
+        dataType: 'json',
+        //"success" gets called when the returned code is a "200" (successfull request). "error" gets called whenever another code is returned (e.g. 404, 500).
+        success: function(res){
+            //No caldria aquesta instrucció perque ja guaredem els que ens convé en sessionStorage
+         //   data['method'] = res;
+        //->     last_fm_data[method] = res;
+           // var  myresposta = JSON.parse(res);
+            //console.log("Resposta: Name " + res.album.name);// Should return session key.
+            //console.log("Resposta: Key " + res.album.mbid);
+            //console.log(res.topalbums.album[0]);
+            //nomAlbum = res.album.name;
+
+            //mbidAlbum = res.album.mbid;
+            let string = null;
+            let foto = null;
+            for (var i=0; i< 10; i++)
+            {
+              console.log(res.topalbums.album[i]);
+              //console.log(res.topalbums.album[i].image);
+              //$("#nomAlbum").text("Nom: "+res.topalbums.album[i].name);
+              string += "Nom: "+res.topalbums.album[i].name;
+              foto += "<img src=\" "+res.topalbums.album[i].image[2]+"\" size=\"medium\">";
+
+            }
+            $("#nomAlbum").text(string);
+            //sessionStorage.setItem("mySessionUser", res.album.name);
+            //sessionStorage.setItem("mySessionKey", res.album.mbid);
+            //$("#nomAlbum").text("Nom: "+res.album.name);
+            //$("#mbidAlbum").text("MBID: "+res.album.mbid);
+
+            //store session key for further authenticate operations...
+
         },
         error : function(xhr, status, error){
             var errorMessage = xhr.status + ': ' + xhr.statusText
