572e2a5c4623be423632baa70cfaea4293577c92
mmontoy4 <mmontoya@almata.cat>
2021-05-28 00:15:20 +0200

    Pulint codi.

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@
   			/*
   			params api_key ( my api key)
   			cb the web that goes when user is authenticated relative path ( depends on the server is launched): http://localhost:3000/mainpage.ht*/
-  			var url= 'http://www.last.fm/api/auth/?api_key=062e9a51720da9c3c4e2bce4aaf4e5ab&cb=http://localhost:3000/principal.html';
+  			var url= 'http://www.last.fm/api/auth/?api_key=062e9a51720da9c3c4e2bce4aaf4e5ab&cb=http://localhost:3000/lastfm/principal.html';
 
   			window.location.replace(url);
   }
